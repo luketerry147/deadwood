e8ccb4e24b29988991574296ce782f7e46d65194
luketerry147 <luketerry147@gmail.com>
2016-02-18 10:55:45 -0800

    Update and rename player.java to Player.java
    Updated move, added constructor

--- Player.java
+++ Player.java
@@ -1,4 +1,7 @@
-public class player{
+public class Player implements Dice{
+   
+   public Player(){
+   }
    private int rank;
    private int money;
    private int credits;
@@ -6,11 +9,14 @@ public class player{
    private int rehearsalcount;
    private Room position;
    private boolean turnOver = false;
+
    public int getRank(){
       return rank;
    }
    private void setRank(newrank){
       this.rank = newrank;
+   }
+
    }
    public int addMoney(int pay){
       return (this.money + pay);
@@ -25,12 +31,32 @@ public class player{
       turnOver = true;
       return (rehearsalcount + 1);      
    }
-   public Room move(){
+   public void move(){
+      string input;
+      boolean inputGood = false;
       turnOver = true;
-      //get adjacent rooms (2 element array of Rooms)
+      Room[] moveOptions = position.getAdjacentRooms();
+      int numAdjRooms = moveOptions.length();
+      System.out.println("The rooms you can go to are:")
+      for(int i = 0; i < numAdjRooms; i++){
+         System.out.println( "\t", moveOptions[i].getName())
+      }
       //ask which rooms to go to
-      //change player's position to new room
-      //or ask again if wrong input
+      while(!inputGood){
+         for(int i = 0; i < numAdjRooms; i++){
+            if(strcmp(input, moveOptions[i].getName())){
+               inputGood = true;
+            }
+         }
+         System.out.println("That wasn't good input, the adjacent rooms to choose from are:")
+         for(int i = 0; i < numAdjRooms; i++){
+            System.out.println("\t", moveOptions[i].getName());
+         }
+      }
+      this.position = moveOptions[i];
+      if(strcmp(this.position.getRoomType(), "Scene")){
+         Role roleTaken = chooseRole(this.position);
+      }
    }
    public int upgrRank(int newrank){
       int inputGood = 1;
@@ -54,6 +80,9 @@ public class player{
         }
     }
   }
+   public int rollDice(int numDice){
+      
+   }
    
    
 }
